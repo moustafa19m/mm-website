1931875cb258cecd29f3a17020ea576a6cd5ba34
Moustafa Makhlouf <moustafa_makhlouf@brown.edu>
2020-11-13 20:00:17 -0500

    delayed info

--- src/components/MmInfo.js
+++ src/components/MmInfo.js
@@ -1,46 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import "../css/Intro.css"
 
 
 
 
-function MmInfo({ scrollDown }) {
+class MmInfo extends React.Component {
 
-    const [offsetY, setOffsetY] = useState(0);
-    const handleScroll = () => {
-        setOffsetY(window.pageYOffset);
+    // const [offsetY, setOffsetY] = useState(0);
+    // const handleScroll = () => {
+    //     setOffsetY(window.pageYOffset);
+    // }
+
+    // useEffect(() => {
+    //     window.addEventListener("scroll", handleScroll);
+    //     console.log(offsetY);
+    //     return () => window.removeEventListener("scroll", handleScroll)
+    // }, []);
+    constructor() {
+        super();
+        this.state = {hidden : true};
     }
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        console.log(offsetY);
-        return () => window.removeEventListener("scroll", handleScroll)
-    }, []);
+    componentWillMount() {
+        setTimeout(() => {
+            this.setState({hidden: false})
+          }, 700);
+    }
 
 
-    return (
-        <Container className="infoCont" fluid="lg">
-            <Row className="row">
-                {/* <Col> */}
-                    <img className='mmImage mm-link' src="../../mm_photo_2.png"/>
-                {/* </Col> */}
-                <Col className="mmInfo" xs="6">
-                    <text style={{color: "white"}}>Hi, my name is </text><br/>
-                    <h1 className="mmName yellowText"><strong>Moustafa Makhlouf</strong></h1>
-                    
-                    <h3 className="cheesyTitle">I speak to computers in their language!</h3>
-                    <br/>
+    render () {
+        return (
+            <Container className="infoCont" fluid="lg">
+            {
+                this.state.hidden ?
 
-                    <p className="shortIntro">
-                        I am a senior at <strong className="yellowText">Brown University</strong>, studying computer science Sc.B. I have experience developing full-stack software, internal development tools, and machine learning models.
-                    </p>
-                </Col>
-            </Row>
-            
-            
-        </Container>
-    );
+                <div></div>
+
+                :
+                
+                    <Row className="row">
+                        {/* <Col> */}
+                            <img className='mmImage mm-link' src="../../mm_photo_2.png"/>
+                        {/* </Col> */}
+                        <Col className="mmInfo" xs="6">
+                            <text style={{color: "white"}}>Hi, my name is </text><br/>
+                            <h1 className="mmName yellowText"><strong>Moustafa Makhlouf</strong></h1>
+                            
+                            <h3 className="cheesyTitle">I speak to computers in their language!</h3>
+                            <br/>
+
+                            <p className="shortIntro">
+                                I am a senior at <strong className="yellowText">Brown University</strong>, studying computer science Sc.B. I have experience developing full-stack software, internal development tools, and machine learning models.
+                            </p>
+                        </Col>
+                    </Row>
+                
+
+            }
+            </Container>
+        
+        );
+    }
   }
 
 export default MmInfo;
